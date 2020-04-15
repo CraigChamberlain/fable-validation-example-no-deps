f40021ad381fb544e56ae340366371961c142e86
Craig <craig@RedHpDebian>
2020-04-15 07:47:45 +0100

    Make errors Optional list to handle unvalidated

--- src/App.fs
+++ src/App.fs
@@ -8,13 +8,16 @@ open Fable.FontAwesome
 
 type ValidationErrors =
     {
-      Email : string list
-      Password : string list
+      Email :  string list option
+      Password : string list option
     } with
+    //Helper to clean up templating.  Would be more idomatic to have a Validation Module.
     member this.HasErrors =
-        not (this.Email = List.empty && this.Password = List.empty)
-
-    static member empty = { Email = []; Password = []}
+        match this.Email, this.Password with
+        | Some [], Some [] -> false
+        | _ -> true // Assume error if unchecked
+    // Initial State
+    static member Unchecked = { Email = None; Password = None}
 
 type Model =
     {
@@ -33,7 +36,7 @@ type Msg =
     | Clear
     | ToggleModal
 
-let init _ = { Email = ""; Password = "" ; Errors = ValidationErrors.empty; ModalState = false}, Cmd.none
+let init _ = { Email = ""; Password = "" ; Errors = ValidationErrors.Unchecked; ModalState = false}, Cmd.none
 
 let validateEmail email =
       [ System.String.IsNullOrWhiteSpace(email), "Field Email cannot be empty"
@@ -58,11 +61,11 @@ let private update msg model =
         { model with Email = newEmail }, Cmd.ofMsg ValidateEmail
     | ValidatePassword  ->
         let passwordErrors = validatePassword model.Password
-        let errors = { model.Errors with Password = passwordErrors }
+        let errors = { model.Errors with Password = Some passwordErrors }
         { model with Errors = errors  }, Cmd.none
     | ValidateEmail  ->
         let emailErrors = validateEmail model.Email
-        let errors = { model.Errors with Email = emailErrors }
+        let errors = { model.Errors with Email = Some emailErrors }
         { model with Errors = errors  }, Cmd.none
     | Submit ->
         { model with ModalState = not model.ModalState }, Cmd.none
@@ -99,23 +102,26 @@ let private emailInput model dispatch =
                 [ Input.email [ Input.OnChange (fun ev -> dispatch (ChangeEmail ev.Value))
                                 Input.Value model.Email
                                 match errors with
-                                | [] -> Input.Color IsSuccess
-                                | _ -> Input.Color IsDanger
+                                | Some [] -> Input.Color IsSuccess
+                                | Some _ -> Input.Color IsDanger
+                                | None -> ()
                                ]
                   Icon.icon [ Icon.Size IsSmall; Icon.IsLeft ]
                     [ Fa.i [ Fa.Solid.Envelope ]
                         [ ] ]
                   Icon.icon [ Icon.Size IsSmall; Icon.IsRight ]
                     [ Fa.i [ match errors with
-                               | [] -> Fa.Solid.Check
-                               | _ -> Fa.Solid.ExclamationTriangle ]
+                               | Some [] -> Fa.Solid.Check
+                               | Some _ -> Fa.Solid.ExclamationTriangle
+                               | None -> ()]
                         [ ] ] ]
               match errors with
-              | [] ->
+              | Some [] ->
                   Help.help  [ Help.Color IsSuccess ] [ str "This Email is valid" ]
-              | errorList ->
+              | Some errorList ->
                   let errorListHtml = ul [] (errorList |> List.map (fun e -> li [ ] [ str e ]) )
                   Help.help  [ Help.Color IsDanger ] [errorListHtml]
+              | _ -> ()
             ]
 
 let private passwordInput model dispatch =
@@ -131,8 +137,9 @@ let private passwordInput model dispatch =
                              Input.Value model.Password
                              Input.Props [ AutoFocus true ]
                              match errors with
-                             | [] -> Input.Color IsSuccess
-                             | _ -> Input.Color IsDanger
+                             | Some [] -> Input.Color IsSuccess
+                             | Some _ -> Input.Color IsDanger
+                             | None -> ()
                           ]
                   Icon.icon [ Icon.Size IsSmall; Icon.IsLeft ]
                     [ Fa.i [ Fa.Solid.User ]
@@ -140,18 +147,20 @@ let private passwordInput model dispatch =
                   Icon.icon [ Icon.Size IsSmall; Icon.IsRight ]
                     [ Fa.i [
                               match errors with
-                               | [] -> Fa.Solid.Check
-                               | _ -> Fa.Solid.ExclamationTriangle
+                               | Some [] -> Fa.Solid.Check
+                               | Some _ -> Fa.Solid.ExclamationTriangle
+                               | None -> ()
                          ]
                         [ ] ] ]
 
 
               match errors with
-              | [] ->
+              | Some [] ->
                   Help.help  [ Help.Color IsSuccess ] [ str "This Password is valid" ]
-              | errorList ->
+              | Some errorList ->
                   let errorListHtml = ul [] (errorList |> List.map (fun e -> li [ ] [ str e ]) )
                   Help.help  [ Help.Color IsDanger ] [errorListHtml]
+              | _ -> ()
             ]
 
 
